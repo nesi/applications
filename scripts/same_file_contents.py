eb6b19dec449bbcce9bd88cfb44f29dd846a4060
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2013-07-16 14:45:45 +1200

    Details for diffs when test fails

--- scripts/same_file_contents.py
+++ scripts/same_file_contents.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import sys
+import difflib
+
 
 filename_1 = sys.argv[1]
 filename_2 = sys.argv[2]
@@ -8,13 +10,21 @@ filename_2 = sys.argv[2]
 f1 = open(filename_1, 'r')
 f2 = open(filename_2, 'r')
 
-if f1.read() == f2.read():
+
+f1content = f1.read()
+f2content = f2.read()
+
+print type(f1content)
+
+if f1content == f2content:
     print "'File contents (%s, %s) are equal'" % (filename_1, filename_2)
     f1.close()
     f2.close()
     sys.exit(0)
 else:
     print "'File contents (%s, %s) are not equal'" % (filename_1, filename_2)
+    result = difflib.unified_diff(f1content.split('\n'), f2content.split('\n'), lineterm='')
+    print '\n'.join(list(result))
     f1.close()
     f2.close()
     sys.exit(1)
