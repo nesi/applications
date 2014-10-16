837b30292fc07faa3427558306e020a45f5c53df
Bart Verleye <b.verleye@auckland.ac.nz>
2014-10-16 14:39:08 +1300

    Adding new software and versions

--- eb.lua/modules/Auckland/pan/eb.lua
+++ eb.lua/modules/Auckland/pan/eb.lua
@@ -3,34 +3,43 @@ local OS = 'RHEL6.3'
 
 local io = require "io"
 
-whatis('Make available the eb command and the modules built with EasyBuild')
-
-family('architecture')
+whatis('Make available the eb command for building with EasyBuild')
+add_property("lmod", "sticky")  -- so "purge --force" required
 
 local HARDWARE='sandybridge'
 for line in io.lines('/proc/cpuinfo') do
 	if line:sub(1,5) == 'flags' then 
-	    if not line:find('avx') then 
+	   if not line:find('avx') then 
 			HARDWARE = 'westmere'
 			break
 		end
 	end
 end
-
 local prefix = pathJoin(EB, OS, HARDWARE)
-local pylib = capture([[
-python -c "import distutils.sysconfig;print distutils.sysconfig.get_python_lib(prefix='/share/easybuild/installation');"
-]])
 
+if not os.getenv('MODULEPATH'):find(prefix) then
+   LmodError("MODULEPATH doesn't match detected architecture ", HARDWARE, ' on ', capture('hostname'))
+end
+
+-- Make eb findable
+local pylib = capture([[
+/usr/bin/python2 -c "import distutils.sysconfig;print distutils.sysconfig.get_python_lib(prefix='/share/easybuild/installation');"
+]])
 prepend_path('PYTHONPATH', pylib)
 prepend_path('PATH', pathJoin(EB, 'installation/bin/'))
-prepend_path('MODULEPATH', pathJoin(prefix, 'modules/all'))
-prepend_path('LMOD_DEFAULT_MODULEPATH', pathJoin(prefix, 'modules/all'))
-setenv('EASYBUILDBUILDPATH', pathJoin(prefix, 'build'))
-setenv('EASYBUILDINSTALLPATH', prefix)
-setenv('EASYBUILD_PREFIX', prefix)
+
+-- Other sharable EB settings for fetch, search etc.
 setenv('EASYBUILD_SOURCEPATH', pathJoin(EB, 'src'))
+setenv('EASYBUILD_PREFIX', prefix)
 setenv('EASYBUILD_MODULES_TOOL', 'Lmod')
 
-
+-- Unsharable EB build and install settings
+if os.getenv('USER') ~= 'nesi.apps' then
+   local home = os.getenv('HOME')
+   prefix = pathJoin(home, 'easybuildinstall', HARDWARE)
+end
+setenv('EASYBUILD_INSTALLPATH', prefix)
+setenv('EASYBUILD_BUILDPATH', pathJoin(prefix, 'build'))
+setenv('EASYBUILD_TMP_LOGDIR', pathJoin(prefix, 'log'))
+   
 
